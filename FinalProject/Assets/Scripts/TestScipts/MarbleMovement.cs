2e65d6c1c103ef2adbbaed25a36417a351535728
Student <student@DM010.local>
2019-11-07 08:49:45 -0600

    gitsubmitagain

--- FinalProject/Assets/Scripts/TestScipts/MarbleMovement.cs
+++ FinalProject/Assets/Scripts/TestScipts/MarbleMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MarbleMovement : MonoBehaviour
 {
@@ -13,11 +14,14 @@ public class MarbleMovement : MonoBehaviour
 
     public bool debug = true;
     public float speed = 10;
+    Vector3 calibratedDir;
+    
     // Start is called before the first frame update
     void Start()
     {
         speed *= 1.5f;
         rb = this.GetComponent<Rigidbody>();
+        Calibrate();
     }
 
     // Update is called once per frame
@@ -25,8 +29,8 @@ public class MarbleMovement : MonoBehaviour
     {
         // Vector3 dir = Vector3.zero; // new Vector 3(0,0,0)l=;
 
-        dir.x = Input.acceleration.x;
-        dir.y = Input.acceleration.y;
+        dir.x = Input.acceleration.x - calibratedDir.x;
+        dir.y = Input.acceleration.y - calibratedDir.z;
         
 
         if(debug){
@@ -35,6 +39,11 @@ public class MarbleMovement : MonoBehaviour
         
         
 
+    }
+    public void Calibrate(){
+        calibratedDir.x = Input.acceleration.x;
+        calibratedDir.y = Input.acceleration.y;
+        Debug.Log("Calibrated Dir = " + calibratedDir);
     }
     void LateUpdate(){
         arrowIndicator.rotation = Quaternion.LookRotation(dir, Vector3.up);
--- FinalProject/Assets/Scripts/TestScipts/TiltingPlatform.cs
+++ FinalProject/Assets/Scripts/TestScipts/TiltingPlatform.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class TiltingPlatform : MonoBehaviour
 {
+    Vector3 calibratedDir;
+    Transform body;
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = this.transform.GetChild(0);
+        Calibrate();
     }
 
+    public void Calibrate(){
+        calibratedDir.x = Input.acceleration.x;
+        calibratedDir.y = Input.acceleration.y;
+        Debug.Log("Calibrated Dir = " + calibratedDir);
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 dir = Vector3.zero;
+
+        dir.x = Input.acceleration.x - calibratedDir.x;
+        dir.y = Input.acceleration.y - calibratedDir.y;
     }
 }
