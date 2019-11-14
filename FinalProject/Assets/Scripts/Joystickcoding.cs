74b3d3d39b5d36a1d5e887c2a8ebf5d5f8b25056
Student <student@DM010.local>
2019-11-14 10:40:01 -0600

    gitsubmit3

--- FinalProject/Assets/Scripts/Joystickcoding.cs
+++ FinalProject/Assets/Scripts/Joystickcoding.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Joystickcoding : MonoBehaviour
 {
+    protected Joystick joystick;
     // Start is called before the first frame update
     void Start()
     {
-        
+        joystick = FindObjectOfType<Joystick>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = new Vector3(joystick.Horizontal * 20f + Input.GetAxis("Horizontal") * 20f, rigidbody.velocity.y, joystick.Vertical * 20f + Input.GetAxis("Vertical") * 20f );
+    }
+
+    void TelePort(){
+        // this.transform.Translate(dir * 3);
+    }
+
+    public Camera mainCam;
+    void OnTriggerEnter (Collider other){
+        if (other.gameObject.CompareTag("CustomCam")){
+            mainCam.gameObject.SetActive(false);
+            other.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+    void OnTriggerExit(Collider other){
+        if(other.gameObject.CompareTag("CustomCam")){
+            mainCam.gameObject.SetActive(true);
+            other.transform.GetChild(0).gameObject.SetActive(false);
+        }
     }
 }
---
+++ FinalProject/Assets/Scripts/Teleport.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+   public Transform destination;
+
+   void OnTriggerEnter(Collider other){
+       if(other.gameObject.CompareTag("Player")){
+           other.transform.position = destination.position;
+       }
+   }
+}
